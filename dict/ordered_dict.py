b110a7f34fe979c148c3d5abc06563126c40db0c
icejoywoo <icejoywoo@gmail.com>
2013-12-24 20:18:59 +0800

    update ordered dict == dict

--- dict/ordered_dict.py
+++ dict/ordered_dict.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 
 ordered_dict = OrderedDict([('first', 1), ('second', 2), ('third', 3)])
 ordered_dict2 = OrderedDict([('third', 3), ('first', 1), ('second', 2)])
-d = dict([('first', 1), ('second', 2), ('third', 3)])
+d = dict([('first', 1), ('third', 3), ('second', 2)])
 
 assert d == ordered_dict
 assert ordered_dict != ordered_dict2
