373f2f4952a0dfb5955abc01821dc9148c5e4d1a
icejoywoo <icejoywoo@gmail.com>
2013-12-25 23:19:13 +0800

    fix get size of the list

--- method/slots_demo.py
+++ method/slots_demo.py
@@ -3,29 +3,45 @@
 # @date: 13-12-25
 
 __author__ = 'icejoywoo'
+import sys
 
 class Person_(object):
     __slots__ = ("name", "age", "gender")
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+    def __init__(self):
+        pass
 
 class Person(object):
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+    def __init__(self):
+        pass
 
 if __name__ == "__main__":
+    class NoSlots(object):
+        pass
+    n = NoSlots
+    class WithSlots(object):
+        __slots__ = 'a', 'b', 'c'
+    w = WithSlots()
+    n.a = n.b = n.c = 23
+    w.a = w.b = w.c = 23
+    print sys.getsizeof(n)
+    print sys.getsizeof(w)
+
     persons = []
-    for i in xrange(500000):
-        persons.append(Person("name_%d"%i, i, "male"))
+    for i in xrange(100000):
+        p = Person()
+        p.name = "name_%d" % i
+        p.age = i
+        p.gender = "female"
+        persons.append(p)
 
     persons_ = []
-    for i in xrange(500000):
-        persons_.append(Person_("name_%d"%i, i, "male"))
+    for i in xrange(100000):
+        p = Person_()
+        p.name = "name_%d" % i
+        p.age = i
+        p.gender = "female"
+        persons_.append(p)
 
-    import sys
-    print "size without slots: %d" % sys.getsizeof(persons)
-    print "size with slots: %d" % sys.getsizeof(persons_)
+    print "size without slots: %d" % sum([sys.getsizeof(p) for p in persons])
+    print "size with slots: %d" % sum([sys.getsizeof(p) for p in persons_])
+
