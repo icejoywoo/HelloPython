5b8bd878511db6f9ff2a13505fca59b3b3071e42
icejoywoo <icejoywoo@gmail.com>
2014-01-08 21:01:34 +0800

    threads demo

--- pitfalls/threads_demo.py
+++ pitfalls/threads_demo.py
@@ -12,11 +12,11 @@ def test_func(id):
     time.sleep(1)
     print('thread %d is running \n' % id)
 
-threads = [threading.Thread(target=test_func, args=(i,)) for i in range(3)]
+threads = [threading.Thread(target=test_func, args=(i,)) for i in range(5)]
 
 for t in threads:
     t.setDaemon(True)
     t.start()
-# for t in threads:
-#     t.join()
+for t in threads:
+    t.join()
 print "[main]\n"
