ccaa068aa25b441bdd0a4e6de2167c94f835e3a1
icejoywoo <icejoywoo@gmail.com>
2014-01-12 13:41:27 +0800

    add  enumerate demo

---
+++ method/enumerate_demo.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+# ^_^ encoding: utf-8 ^_^
+# @date: 14-1-12
+
+__author__ = 'icejoywoo'
+
+a = ["a", "b", "c", "d"]
+
+# simple iterate
+for i in a:
+    print i
+
+# iterate with index
+for i, el in enumerate(a):
+    print i, el
