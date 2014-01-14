23303e5ad2095c4195c1e1f8753c74533e8b5b57
icejoywoo <icejoywoo@gmail.com>
2014-01-14 14:10:35 +0800

    add a filelock for *nix

---
+++ method/filelock.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+#^_^ encoding: utf-8 ^_^
+# @date: 14-1-14
+
+__author__ = 'wujiabin'
+
+from fcntl import flock, LOCK_EX, LOCK_UN, LOCK_NB
+
+class LockFile(object):
+    def __init__(self, path, mod):
+        self.fd = open(path, mod)
+
+    def __enter__(self):
+        try:
+            flock(self.fd, LOCK_EX | LOCK_NB)
+            return self.fd
+        except Exception as e:
+            self.fd.close()
+            raise e
+
+    def __exit__(self, *args, **kwargs):
+        flock(self.fd, LOCK_UN)
+        self.fd.close()
+
+
+if __name__ == '__main__':
+    import time
+    import os
+    with LockFile('xx', 'w+') as f:
+        f.write('blah blah blah')
+        time.sleep(3)
+    print 'woo~~ ', os.getpid()
