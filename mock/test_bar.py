97a4f670b804dc82b6a2e9c76f1c7c8652c10242
icejoywoo <icejoywoo@gmail.com>
2014-01-26 16:48:17 +0800

    update

--- mock/test_bar.py
+++ mock/test_bar.py
@@ -4,3 +4,19 @@
 
 __author__ = 'wujiabin'
 
+from bar import Coin, Wallet
+from mock import *
+
+
+def setUp():
+    print "setup"
+
+
+def tearDown():
+    print "teardown"
+
+
+def test_bar1():
+    a = Coin('dollar', 100)
+    a.__str__ = Mock(return_value="__str__ magic method")
+    assert str(a) == "__str__ magic method"
