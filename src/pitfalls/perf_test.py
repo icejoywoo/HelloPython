1f5b20aa72a4685199481b68a2e58f5498b776dd
icejoywoo <icejoywoo@gmail.com>
2014-02-13 21:03:55 +0800

    add a performance comparison

---
+++ src/pitfalls/perf_test.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+#^_^ encoding: utf-8 ^_^
+# @date: 14-2-13
+
+__author__ = 'wujiabin'
+# http://blog.jobbole.com/23773/
+
+import time
+
+
+class Timer(object):
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, exception_type, exception_val, trace):
+        print "elapsed:", time.time() - self.start
+
+
+def main():
+    for _ in xrange(10**8):
+        pass
+
+with Timer():
+    main()
+
+with Timer():
+    for _ in xrange(10**8):
+        pass
+
