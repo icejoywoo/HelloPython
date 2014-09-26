9792afb9d2ffe44e8e1caa9e08ec62c940ba33cf
icejoywoo <icejoywoo@gmail.com>
2014-09-26 13:38:45 +0800

    add a bounded_dict

---
+++ src/lab/bounded_dict.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+# ^_^ encoding: utf-8 ^_^
+# @date: 2014/8/19
+
+__author__ = 'wujiabin'
+
+import collections
+
+
+class MyDict(collections.MutableMapping):
+    def __init__(self, maxlen, *a, **k):
+        self.maxlen = maxlen
+        self.d = dict(*a, **k)
+        while len(self) > maxlen:
+            self.popitem()
+
+    def __iter__(self):
+        return iter(self.d)
+
+    def __len__(self):
+        return len(self.d)
+
+    def __getitem__(self, k):
+        return self.d[k]
+
+    def __delitem__(self, k):
+        del self.d[k]
+
+    def __setitem__(self, k, v):
+        if k not in self and len(self) == self.maxlen:
+            self.popitem()
+        self.d[k] = v
+
+
+d = MyDict(5)
+for i in range(10):
+    d[i] = i
+    print sorted(d)
