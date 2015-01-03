ed10884627710ab1de2a76e4c77b10b9b848d4f9
icejoywoo <icejoywoo@gmail.com>
2015-01-03 17:42:35 +0800

    add fp demo

---
+++ src/lab/fp_demo.py
@@ -0,0 +1,76 @@
+#!/bin/env python
+# encoding: utf-8
+# @date: 15-1-3
+
+__author__ = 'icejoywoo'
+
+
+def fp_hd(l):
+    return l[0]
+
+
+def fp_tl(l):
+    return l[1:]
+
+
+def fp_map(f, l):
+    """ fp-like map
+    :param f:
+    :param l:
+    :return:
+    """
+    if l:
+        return [f(fp_hd(l))] + fp_map(f, fp_tl(l))
+    else:
+        return []
+
+
+print fp_map(lambda x: x + 1, [1, 2, 3])
+
+
+def fp_fold(f, acc, l):
+    if l:
+        return f(acc, fp_hd(l)) + fp_fold(f, acc,  fp_tl(l))
+    else:
+        return acc
+
+print fp_fold(lambda acc, x: acc + x, 0, [1, 2, 3])
+
+
+def fp_foldr(f, acc, l):
+    if l:
+        return f(acc, fp_hd(l)) + fp_fold(f, acc,  fp_tl(l))
+    else:
+        return acc
+
+
+def fp_filter(f, l):
+    if l:
+        head = fp_hd(l)
+        if f(head):
+            return [head] + fp_filter(f, fp_tl(l))
+        else:
+            return fp_filter(f, fp_tl(l))
+    else:
+        return []
+
+print fp_filter(lambda x: x > 0, [-1, 3, 0, -4, 5, 9])
+
+
+def fp_currying(f, *args, **kwargs):
+    saved_args = list(args)
+    saved_kwargs = kwargs
+
+    def _f(*_args, **_kwargs):
+        saved_kwargs.update(_kwargs)
+        return f(*tuple(saved_args + list(_args)), **saved_kwargs)
+
+    return _f
+
+
+def t(a, b, c, d=1, f=2):
+    return a + b + c + d + f
+
+fp_curried_t = fp_currying(t, 1, 2, d=3)
+
+print fp_curried_t(4, f=3)
